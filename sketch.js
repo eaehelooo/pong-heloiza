620d7deed65834daf236e080e3b6cbe8276f16c0
eaehelooo <106965999+eaehelooo@users.noreply.github.com>
2022-06-06 08:19:12 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600, 400);
+}
+let xbola=300
+let ybola=200
+let diametro=30
+let velocidadexbola=6
+let velocidadeybola=6
+function draw() {
+  background(0); 
+circle(xbola,ybola,diametro);
+xbola=xbola+6;
+ ybola=ybola=6;
+if(xbola>600)[velocidade=xbola=velocidadexbola*-1]
+}
